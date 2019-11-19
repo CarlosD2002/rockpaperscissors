f49e16dbee2a2fc889a0e965e0af0e5b191e20c2
CarlosD2002 <c.wuchen@ihsph.org>
2019-11-19 23:00:58 +0000

    add a click handler

--- js/script.js
+++ js/script.js
@@ -5,7 +5,13 @@
 //GLOBAL VARIABLES
 /* global $ */
 
+//let random = Math.floor(Math.random() * 3) + 1);
 
 
 // DOCUMENT READY FUNCTION BELOW
-
+$( "document" ).ready(function() { 
+    $("#shoot").click(function() {
+        let userChoice = $("#input").val();
+        $("#result").append(`<p>${userChoice}</p>`);
+    });
+});
