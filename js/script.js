12df7546de5532650f342bf83eb12c20de3bd9eb
CarlosD2002 <c.wuchen@ihsph.org>
2019-11-26 23:16:31 +0000

    Today I finished the work, I made the computer choice, display it on the html and I finish the comparation to choose the winner

--- js/script.js
+++ js/script.js
@@ -9,10 +9,47 @@
 
 $("document").ready(function() { 
     $("#shoot").click(function() {
-        let userChoice = $("#input").val();
-        $("#result").append(`<p>${userChoice}</p>`);
-        let random = Math.floor(Math.random() * 3);
+        $("#input").empty();
+        $("#userChoice").empty();
+        $("computerChoice").empty();
+        $("#result").empty();
+        let userInput = $("#input").val();
+        $("#userChoice").text(`${userInput}`);
         let possible_outcomes = ['rock', 'paper', 'scissors'];
-        possible_outcomes.length
+        let computerRandom = (Math.floor(Math.random() * 3) + 1);
+        if(computerRandom === 1) {
+            $("#computerChoice").text(`${possible_outcomes[0]}`)
+        } else if(computerRandom === 2) {
+            $("#computerChoice").text(`${possible_outcomes[1]}`)
+        } else{
+            $("#computerChoice").text(`${possible_outcomes[2]}`)
+        }
+if((userInput === "rock") && (computerRandom === 3)) {
+    $("#result").text("You are the winner!");
+}
+else if((userInput === "paper") && (computerRandom === 1)) {
+    $("#result").text("You are the winner!");
+}
+else if((userInput === "scissors") && (computerRandom === 2)) {
+    $("#result").text("You are the winner!");
+}
+else if((userInput === "rock") && (computerRandom === 1)) {
+    $("#result").text("You draw!");
+}
+else if((userInput === "paper") && (computerRandom === 2)) {
+    $("#result").text("You draw!");
+}
+else if((userInput === "scissors") && (computerRandom === 3)) {
+    $("#result").text("You draw!");
+}
+else if((userInput === "rock") && (computerRandom === 2)) {
+    $("#result").text("You lost!");
+}
+else if((userInput === "paper") && (computerRandom === 3)) {
+    $("#result").text("You lost!");
+}
+else if((userInput === "scissors") && (computerRandom === 1)) {
+    $("#result").text("You lost!");
+}
     });
 });
